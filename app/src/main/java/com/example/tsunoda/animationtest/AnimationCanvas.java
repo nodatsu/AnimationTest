0ce3a680a6f977de70a6daccf6ad9887d5c1a77a
H.Tsunoda <tsunoda@aomori-u.ac.jp>
2015-11-08 14:05:26 +0900

    円を描画、タイマーで移動、landscape固定

--- app/src/main/java/com/example/tsunoda/animationtest/AnimationCanvas.java
+++ app/src/main/java/com/example/tsunoda/animationtest/AnimationCanvas.java
@@ -5,17 +5,71 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.view.View;
 
+import android.os.Handler;
+import android.os.Message;
+
 public class AnimationCanvas extends View {
     private Paint mPaint = new Paint();
 
+    private int posX, posY;
+    private int velX, velY;
+    private boolean isAttached;
+    private static final long DELAY_MILLIS = 60;
+
     public AnimationCanvas(Context context) {
         super(context);
+
+        posX = 150;
+        posY = 150;
+        velX = 2;
+        velY = 1;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawText("Hello Custom View!", 50, 50, mPaint);
+        canvas.drawText("Animation Canvas", 50, 50, mPaint);
+
+        canvas.drawCircle(posX, posY, 25, mPaint);
+    }
+
+    private void move() {
+        posX += velX;
+        posY += velY;
+    }
+
+    /**
+     * タイマーハンドラー
+     */
+    private Handler myHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (isAttached) {
+                // 移動処理
+                move();
+
+                // 再描画
+                invalidate();
+                sendEmptyMessageDelayed(0, DELAY_MILLIS);
+            }
+        }
+    };
+
+    /**
+     * WindowにAttachされた時の処理
+     */
+    protected void onAttachedToWindow() {
+        isAttached = true;
+        myHandler.sendEmptyMessageDelayed(0, DELAY_MILLIS);
+        super.onAttachedToWindow();
+    }
+
+    /**
+     * WindowからDetachされた時の処理
+     */
+    protected void onDetachedFromWindow() {
+        isAttached = false;
+        super.onDetachedFromWindow();
     }
 }
