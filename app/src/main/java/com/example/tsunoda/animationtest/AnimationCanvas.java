31385a9aa3ee69a3a831bdace1f53f60cca5105b
H.Tsunoda <tsunoda@aomori-u.ac.jp>
2015-11-08 13:18:19 +0900

    refactoring

--- app/src/main/java/com/example/tsunoda/animationtest/AnimationCanvas.java
+++ app/src/main/java/com/example/tsunoda/animationtest/AnimationCanvas.java
@@ -1,9 +1,5 @@
 package com.example.tsunoda.animationtest;
 
-/**
- * Created by tsunoda on 2015/11/08.
- */
-
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
